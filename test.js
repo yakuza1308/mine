7d2c64b04a29b39b56e4ecc133d781c1686a8aba
Ainurrochman <yakuza1308@gmail.com>
2018-10-02 12:13:32 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,150 @@
+{
+    "_id" : ObjectId("5bacb26e625c057f5329add9"),
+    "InitiativeID" : "4032",
+    "TrackingID" : "AP102",
+    "ProjectName" : "AP102 - Strengthen interaction and awarenesss with the business on FCC related topics",
+    "LifeCycleId" : "LC4",
+    "SubLifeCycleId" : "",
+    "SCCategory" : "TBD1",
+    "BusinessDriverId" : "",
+    "BusinessDriverImpact" : "Primary",
+    "CBLedInitiatives" : false,
+    "ProductAssignment" : [ 
+        "5b39f60ba7da87c5e8da382b"
+    ],
+    "Functions" : [ 
+        "5b39f57ca7da87c5e8da381e"
+    ],
+    "PrincipleRisk" : [ 
+        "5b568bc3dfd3d52632e94b64"
+    ],
+    "RiskSubtype" : [ 
+        "5b568bc3dfd3d52632e94b65", 
+        "5b568bc3dfd3d52632e94b66", 
+        "5b568bc3dfd3d52632e94b67"
+    ],
+    "ConductTheme" : [ 
+        "5b39f983a7da87c5e8da3842", 
+        "5b39f984a7da87c5e8da384c"
+    ],
+    "ConductSubtype" : [ 
+        "5b4d9f48eaad0e985e76f5b4", 
+        "5b4d9f48eaad0e985e76f5b5", 
+        "5b4d9f48eaad0e985e76f5ac"
+    ],
+    "InitiativeCategory" : [ 
+        "5b4da21beaad0e985e76f5cd"
+    ],
+    "InitiativeSubCategory" : [],
+    "MapCategory" : "",
+    "MapCriticalityRating" : "N/A",
+    "MapSource" : "",
+    "Segment" : [ 
+        "5b4c070501112617537cc93d", 
+        "5b4c070601112617537cc93e", 
+        "5b4c070601112617537cc940"
+    ],
+    "SubSegment" : [ 
+        "5b569159dfd3d52632e94b83", 
+        "5b569159dfd3d52632e94b84", 
+        "5b569159dfd3d52632e94b85", 
+        "5b569159dfd3d52632e94b86", 
+        "5b569159dfd3d52632e94b87", 
+        "5b569159dfd3d52632e94b88", 
+        "5b569159dfd3d52632e94b89", 
+        "5b569159dfd3d52632e94b8a", 
+        "5b569159dfd3d52632e94b8b", 
+        "5b569159dfd3d52632e94b8c", 
+        "5b569159dfd3d52632e94b8d", 
+        "5b569159dfd3d52632e94b8e", 
+        "5b569159dfd3d52632e94b8f", 
+        "5b88eb0de7e315d27dade29a"
+    ],
+    "FiveShifts" : [],
+    "EX" : false,
+    "OE" : false,
+    "Tag1" : false,
+    "Tag2" : false,
+    "Tag3" : false,
+    "Tag4" : false,
+    "Tag5" : false,
+    "Tag6" : true,
+    "Tag7" : false,
+    "Tag8" : false,
+    "Tag9" : false,
+    "Tag10" : false,
+    "StartDate" : ISODate("2018-01-01T00:00:00.000Z"),
+    "FinishDate" : ISODate("2018-12-31T00:00:00.000Z"),
+    "RevisedEndDate" : Date(-62135596800000),
+    "RevisedEndDateReasonChange" : "",
+    "ProblemStatement" : "NIL",
+    "ProjectDescription" : "Periodic meetings / discussion forums with the business on FCC-related regulatory changes, market insights, or clients and business concerns",
+    "ProjectManager" : "Gary James Wilson",
+    "ProjectManagerID" : [ 
+        "1542576"
+    ],
+    "BusinessImpact" : "Medium",
+    "InvestmentId" : "",
+    "AccountableExecutive" : "Nick Gilbert,Vishal Choraria",
+    "AccountableExecutiveID" : [ 
+        "1192273", 
+        "1334042"
+    ],
+    "TechnologyLead" : "",
+    "TechnologyLeadID" : [],
+    "Stakeholder" : "",
+    "StakeholderID" : [],
+    "ProgressCompletion" : 0.0,
+    "PlannedCost" : 0.0,
+    "ProjectClassification" : "",
+    "Attachments" : [],
+    "DeletedAttachments" : [],
+    "InitiativeType" : "KeyEnablers",
+    "ProjectDriver" : "",
+    "IsGlobal" : false,
+    "Region" : [ 
+        "ASA"
+    ],
+    "Country" : [ 
+        "THAILAND"
+    ],
+    "CommentList" : [ 
+        {
+            "id" : "",
+            "Username" : "1412837",
+            "DateInput" : "2018-08-18T00:00:00.000Z",
+            "Comment" : "To kick off the first meeting in Jan.2018",
+            "updated_date" : ISODate("2018-09-27T10:35:27.891Z"),
+            "updated_by" : "1412837"
+        }
+    ],
+    "type" : "COMPLIANCELED",
+    "SetAsComplete" : false,
+    "CompletedDate" : Date(-62135596800000),
+    "DisplayProgress" : "green",
+    "Sponsor" : "Nick Gilbert",
+    "IsInitiativeTracked" : false,
+    "MetricBenchmark" : "",
+    "AdoptionScoreDenomination" : "",
+    "UsefulResources" : "",
+    "Milestones" : [],
+    "KeyMetrics" : [],
+    "RiskIssues" : [],
+    "ImprovedEfficiencyCurrent" : 0.0,
+    "ImprovedEfficiencyTarget" : 0.0,
+    "ClientExperienceCurrent" : 0.0,
+    "ClientExperienceTarget" : 0.0,
+    "OperationalImprovementCurrent" : 0.0,
+    "OperationalImprovementTarget" : 0.0,
+    "CSRIncreaseCurrent" : 0.0,
+    "CSRIncreaseTarget" : 0.0,
+    "TurnAroundTimeCurrent" : 0.0,
+    "TurnAroundTimeTarget" : 0.0,
+    "updated_date" : ISODate("2018-08-18T00:00:00.000Z"),
+    "updated_by" : "1412837",
+    "RAGComment" : "To kick off the first meeting in Jan.2018",
+    "Status" : "",
+    "IsTask" : false,
+    "BusinessImpactCode" : NumberLong(1),
+    "SearchIndex" : "gary james wilson nick gilbert,vishal choraria ap102 - strengthen interaction and awarenesss with the business on fcc related topics  nick gilbert nil periodic meetings / discussion forums with the business on fcc-related regulatory changes, market insights, or clients and business concerns"
+}
