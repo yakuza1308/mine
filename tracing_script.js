c7364d2082d69c7a7904be6ad9a767122f4e3b2e
Ainurrochman <yakuza1308@gmail.com>
2018-10-06 12:59:26 +0800

    Update tracing_script.js

--- tracing_script.js
+++ tracing_script.js
@@ -1,3 +1,18 @@
 var user_loginid = "1307515" //Geraldine loginid
 var user_group = db.acl_users.findOne({loginid:user_loginid,groups:/.*5a6ec6d877b8a9f91600003a.*/}).groups[0]
-db.acl_groups.find({_id:user_group})
+var is_grantedsd = false;
+db.acl_groups.find({_id:user_group}).forEach(function(x){
+  x.grants.forEach(function(g){
+    if(g.accessid.toLowerCase().indexOf("stake")>=0){
+        is_grantedsd = true;
+    }
+  })  
+})
+var landing_page = ""
+db.acl_accessibility.find({roleid:user_group}).forEach(function(x){
+    if(x.islandingpage){
+        landing_page = x.title
+    }
+})
+print("Granted Access to Stakeholder Dashboard : "+(is_grantedsd ? "YES" : "NO"))
+print("Landing Page : "+landing_page)
