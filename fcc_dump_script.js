8676b04cb4d17b47cc8876be4144942c86ec14dc
Ainurrochman <yakuza1308@gmail.com>
2018-10-06 12:51:52 +0800

    Delete fcc_dump_script.js

--- fcc_dump_script.js
+++
@@ -1,5 +0,0 @@
-db.getCollectionNames().forEach(function(x){
-  if(x.indexOf("tb_5b2b20a35308afe5ab60f2e5") >= 0){
-    print("mongodump --db scbssuat --collection "+x+" --out /hey/its/your_dump_directory")
-  }  
-})
